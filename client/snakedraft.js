20aa7d630969de75f8eb380d13e3d2c3159c6284
pstuart <pstuart2@gmail.com>
2013-01-30 12:42:46 -0600

    Able to add a custom ticket now. Need to be able to add one from Jira.

--- client/snakedraft.js
+++ client/snakedraft.js
@@ -1,15 +1,29 @@
-// Added outside the "if" statement because it is for both the client and server.
 var Tickets = new Meteor.Collection("Tickets");
 
-if (Meteor.isClient) {
+Template.TmplTickets.TicketArr = function() {
+	return Tickets.find({}, {sort: {Id: 1}});
+};
 
-	Template.TmplTickets.TicketArr = function() {
-		return Tickets.find({}, {sort: {Id: 1}});
-	};
+Template.TmplTickets.events = {
+	"click .ticket": function() {
+		alert("You just got ticket: " + this.Id);
+	}
+};
 
-	Template.TmplTickets.events = {
-		"click .ticket": function() {
-			alert("You just got ticket: " + this.Id);
-		}
-	};
-}
+Template.TmplCommandBar.events = {
+	"click button.add-custom-ticket": function() {
+		$("#addTickets").show();
+	},
+	"click button.save-custom-ticket": function() {
+		var ticket = $("#ticket"),
+				title = $("#title"),
+				desc = $("#description");
+
+		Tickets.insert({Id: ticket.val(), Title: title.val(), Description: desc.val()});
+		$("#addTickets").hide();
+
+		ticket.val(null);
+		title.val(null);
+		desc.val(null);
+	}
+};
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,7 +1,1 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: paul
- * Date: 1/30/13
- * Time: 12:27 PM
- * To change this template use File | Settings | File Templates.
- */
+var Tickets = new Meteor.Collection("Tickets");
