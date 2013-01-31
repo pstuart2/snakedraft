12d9a3314b7cc4ee84ce0c786981049d5ea44a6b
pstuart <pstuart2@gmail.com>
2013-01-30 22:36:07 -0600

    Got inline config editworking.

--- client/views/Config.js
+++ client/views/Config.js
@@ -1,8 +1,49 @@
+// When editing a list name, ID of the list
+Session.set('editing_config', null);
+
+////////// Helpers for in-place editing //////////
+
+// Returns an event map that handles the "escape" and "return" keys and
+// "blur" events on a text input (given by selector) and interprets them
+// as "ok" or "cancel".
+var okCancelEvents = function (selector, callbacks) {
+	var ok = callbacks.ok || function () {};
+	var cancel = callbacks.cancel || function () {};
+
+	var events = {};
+	events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+			function (evt) {
+				if (evt.type === "keydown" && evt.which === 27 ||
+						evt.type === "focusout") {
+					// escape = cancel
+					cancel.call(this, evt);
+
+				} else if (evt.type === "keyup" && evt.which === 13) {
+					// blur/return/enter = ok/submit if non-empty
+					var value = String(evt.target.value || "");
+					if (value)
+						ok.call(this, value, evt);
+					else
+						cancel.call(this, evt);
+				}
+			};
+	return events;
+};
+
+var activateInput = function (input) {
+	input.focus();
+	input.select();
+};
+
+Template.Config.editing = function () {
+	return Session.equals('editing_config', this._id);
+};
+
 Template.Config.ConfigsArr = function() {
 	return Configs.find({}, {sort: {Name: 1}});
 };
 
-Template.Config.events = {
+Template.Config.events({
 	"click button.config": function() {
 		$("#configs").toggle();
 	},
@@ -14,5 +55,23 @@ Template.Config.events = {
 
 		name.val(null);
 		value.val(null);
+	},
+	"click .config-item": function (evt, tmpl) { // start editing
+		console.log("Editing Config: " + this.Name);
+		Session.set('editing_config', this._id);
+		Meteor.flush(); // force DOM redraw, so we can focus the edit field
+		activateInput(tmpl.find("#value-input"));
 	}
-};
+});
+
+Template.Config.events(okCancelEvents(
+		'#value-input',
+		{
+			ok: function (value) {
+				Configs.update(this._id, {$set: {Value: value}});
+				Session.set('editing_config', null);
+			},
+			cancel: function () {
+				Session.set('editing_config', null);
+			}
+		}));
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,1 +1,15 @@
+// if the database is empty on server start, create some sample data.
+Meteor.startup(function () {
+	var data = [
+		{Name: "JiraUrl", Value: "http://someurl.com"},
+		{Name: "ScrumMaster", Value: ""}
+	];
 
+	if (Configs.find().count() != data.length) {
+		for (var i = 0; i < data.length; i++) {
+			if (Configs.findOne({Name: data[i].Name}) == null) {
+				Configs.insert(data[i]);
+			}
+		}
+	}
+});
